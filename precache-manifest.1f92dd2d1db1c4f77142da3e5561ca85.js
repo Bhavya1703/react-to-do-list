dbf3b7aee22f855c3c1ae0c3c15d525b63b0c55c
Bhavya Sahu <bhavyaias001@gmail.com>
2020-08-02 18:22:58 +0530

    Updates

---
+++ precache-manifest.1f92dd2d1db1c4f77142da3e5561ca85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da89861b3962236a1517728c6f2870e",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "9a5fdd67a7077a38ff25",
+    "url": "/react-to-do-list/static/css/main.007b49b1.chunk.css"
+  },
+  {
+    "revision": "5e4f8268607a56b644dc",
+    "url": "/react-to-do-list/static/js/2.840efc4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.840efc4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5fdd67a7077a38ff25",
+    "url": "/react-to-do-list/static/js/main.be139d5b.chunk.js"
+  },
+  {
+    "revision": "95a24339b0000315bdb9",
+    "url": "/react-to-do-list/static/js/runtime-main.adc778d4.js"
+  },
+  {
+    "revision": "6f7b19173a67cf188ef02cc5e3f7ee09",
+    "url": "/react-to-do-list/static/media/bg.6f7b1917.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
